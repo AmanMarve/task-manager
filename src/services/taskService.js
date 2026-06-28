63c41aa270fa86104749b3be1aefd7e12ffea9b5
Aman Marve <aman1.marve@gmail.com>
2026-06-28 17:06:33 +0530

    Productin_url added

--- src/services/taskService.js
+++ src/services/taskService.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
 const API_URL = 'http://localhost:5000/api/tasks';
+//Production_URL
+// const API_URL = 'https://task-manager-backend-2-n3r9.onrender.com/api/tasks';
+
 
 export const getTasks = async () => {
     try {
